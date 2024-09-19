cc07f023acbaac48e32de2160731fb2f55273661
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-19 18:39:29 +0500

    style fixed

--- app/GetBlog/page.tsx
+++ app/GetBlog/page.tsx
@@ -4,17 +4,17 @@ import Loader from '@/Loader'
 import { Blog } from '@/util/BlogInterface'
 import React, { useEffect, useState } from 'react'
 
-const AllBlogs = () => {
+const AllBlogs: React.FC = () => {
   const [BlogData, SetBlogData] = useState<Blog[]>([])
-  const [loading, setloading] = useState(false)
+  const [loading, setLoading] = useState<boolean>(false)
 
   const GetAllBlogs = async () => {
-    setloading(true)
+    setLoading(true)
     const Data = await GetBlogs()
     if (Data) {
       SetBlogData(Data)
     }
-    setloading(false)
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -24,11 +24,13 @@ const AllBlogs = () => {
   if (loading) return <Loader />
 
   return (
-    <div className="">
+    <div className="p-8 bg-gray-900 min-h-screen">
       {BlogData.length > 0 ? (
-        BlogData.map((element) => (
-          <BlogCard key={element.id} element={element} />
-        ))
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {BlogData.map((element) => (
+            <BlogCard key={element.id} element={element} />
+          ))}
+        </div>
       ) : (
         <div className="text-center text-gray-500">No data found</div>
       )}
