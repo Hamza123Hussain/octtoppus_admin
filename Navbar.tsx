5d7a4c616c064aa92de556074bb7a252305ddf6c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-19 18:20:19 +0500

    style to be fixed

--- Navbar.tsx
+++ Navbar.tsx
@@ -4,8 +4,17 @@ import React, { useContext, useState, useEffect } from 'react'
 import { UserContext } from './Context'
 import { handleSignOut } from './AUTH/SignOut'
 import { useRouter } from 'next/navigation'
-const Navbar = () => {
+
+const Navbar: React.FC = () => {
   const Router = useRouter()
+  const context = useContext(UserContext)
+  const [isClient, setIsClient] = useState<boolean>(false)
+
+  useEffect(() => {
+    // This ensures that `isClient` is only true on the client side
+    setIsClient(true)
+  }, [])
+
   const SIGNOUT = async () => {
     const success = await handleSignOut()
     if (success) {
@@ -16,34 +25,32 @@ const Navbar = () => {
       Router.push('/Login')
     }
   }
-  const context = useContext(UserContext)
-  const [isClient, setIsClient] = useState(false)
-  useEffect(() => {
-    // This ensures that `isClient` is only true on the client side
-    setIsClient(true)
-  }, [])
 
   if (!isClient) return null
+
   return (
-    <div className="flex flex-col mb-4 justify-between sm:flex-row bg-gray-400 w-full  p-2 px-4 items-center ">
-      <div className=" flex gap-2 items-center">
+    <div className="flex flex-col mb-4 justify-between sm:flex-row bg-gradient-to-r from-purple-600 to-purple-800 text-white p-4 items-center shadow-lg">
+      <div className="flex items-center gap-5">
         <Image
           src={context?.userData?.imageUrl || '/logo.png'} // Fallback to default image if no user image
           alt="Profile Image"
           width={40}
           height={40}
-          className="rounded-full mb-4 sm:mb-0"
+          className="rounded-full border-2 border-gray-200"
         />
-
-        <h2 className="text-xl font-semibold">{context?.userData?.Name}</h2>
+        <h2 className="text-xl font-semibold ml-4">
+          {context?.userData?.Name}
+        </h2>
       </div>
+
       <button
         onClick={SIGNOUT}
-        className="w-23 bg-red-600 text-white p-2 rounded-lg hover:bg-red-500 transition duration-300"
+        className="bg-red-600 text-white p-2 rounded-lg hover:bg-red-500 transition duration-300 mt-4 sm:mt-0"
       >
         Sign Out
       </button>
     </div>
   )
 }
+
 export default Navbar
