563481f0cefdbfb8d561faf70da7b863d2aa0933
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-19 17:07:37 +0500

    styling to be done

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@
 
 import Navbar from '@/Navbar'
 import AllBlogs from './GetBlog/page'
+import AddBlog from './AddNewBlog/page'
 export default function Home() {
   return (
-    <>
+    <div className=" p-4 sm:p-6 md:p-8 rounded-lg shadow-md w-full  px-4">
       <Navbar />
-      <div className=" p-4 sm:p-6 md:p-8 rounded-lg shadow-md w-full  px-4">
-        {/* <Blog /> */}
-        <AllBlogs />
-      </div>
-    </>
+      <AddBlog />
+      <AllBlogs />
+    </div>
   )
 }
