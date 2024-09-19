292bec13cca6092e96e3fd46144921b740e89309
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-19 18:20:23 +0500

    developed

--- condtionalLayout.tsx
+++ condtionalLayout.tsx
@@ -1,42 +1,29 @@
+// components/ConditionalLayout.tsx
 'use client'
-import React, { ReactNode, useEffect, useState, useContext } from 'react'
-import { useRouter, usePathname } from 'next/navigation'
+import { ReactNode, useContext, useEffect } from 'react'
+
+import { usePathname, useRouter } from 'next/navigation'
 import { UserContext } from './Context'
 
 const ConditionalLayout = ({ children }: { children: ReactNode }) => {
-  const context = useContext(UserContext)
-  const [isClient, setIsClient] = useState(false)
   const pathname = usePathname()
   const router = useRouter()
-
+  const context = useContext(UserContext)
+  const isLoggedIn = !!context?.userData?.UserID
   useEffect(() => {
-    setIsClient(true)
-    if (context) {
-      if (!context.userData && !isAuthPage(pathname)) {
-        // Redirect to the login page if not authenticated and not on auth pages
-        router.push('/Login')
-      } else if (context.userData && isAuthPage(pathname)) {
-        // Redirect to the home page or another page if authenticated and trying to access auth pages
-        router.push('/')
+    const authPages = ['/Login', '/Signup', '/Forgot']
+    if (isLoggedIn) {
+      if (authPages.includes(pathname)) {
+        router.push('/') // Redirect to a logged-in page
+      }
+    } else {
+      // If not logged in, redirect to login page if accessing protected pages
+      if (!authPages.includes(pathname)) {
+        router.push('/Login') // Redirect to login page
       }
     }
-  }, [context, pathname, router])
+  }, [isLoggedIn, pathname, router])
 
-  // Define which routes are authentication pages
-  const isAuthPage = (path: string) => {
-    return path === '/Login' || path === '/SignUp' || path == '/Forgot'
-  }
-
-  // Only render on the client side, avoid SSR issues
-  if (!isClient) {
-    return null // You can also return a loading spinner if preferred
-  }
-
-  return (
-    <main className="flex flex-col min-h-screen  mx-auto justify-center items-center text-white">
-      {children}
-    </main>
-  )
+  return <>{children}</>
 }
-
 export default ConditionalLayout
