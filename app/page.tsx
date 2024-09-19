3ff6b61904c3f5568bcfd96b533308ac357eec00
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-19 18:20:57 +0500

    removed padding for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import Navbar from '@/Navbar'
 import AllBlogs from './GetBlog/page'
 import AddBlog from './AddNewBlog/page'
+import Navbar from '@/Navbar'
 export default function Home() {
   return (
-    <div className=" p-4 sm:p-6 md:p-8 rounded-lg shadow-md w-full  px-4">
+    <div className=" p-4 ">
       <Navbar />
       <AddBlog />
       <AllBlogs />
