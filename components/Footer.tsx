32fef0b02192f5ad22e8d6dc249732114c220a1a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-21 13:54:38 +0500

    changing font size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="w-full bg-gradient-to-r from-purple-700 via-purple-800 to-purple-900 text-black py-4 mt-auto">
-      <div className="container mx-auto text-center">
-        <p className="text-sm font-semibold">
+      <div className="mx-auto text-center">
+        <p className="text-xs font-semibold">
           © 2024 Octtoppus. All rights reserved.
         </p>
       </div>
