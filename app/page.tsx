dac36edc2706ab869b953106be2b1007be96b193
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-21 11:50:30 +0500

    not needed here

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 'use client'
-
-import Navbar from '@/components/Navbar'
 import { AddBlog } from './AddNewBlog/page'
 
 export default function Home() {
   return (
     <div className=" p-4 ">
-      <Navbar />
       <AddBlog />
     </div>
   )
