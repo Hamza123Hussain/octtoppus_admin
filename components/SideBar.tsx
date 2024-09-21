fa2dc89d14b612f49794689a4ddca61f72260e01
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-21 13:02:21 +0500

    cleaned code and updated styling

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,28 +1,17 @@
 'use client'
-import { FaHome, FaPlus, FaBox, FaList, FaUsers } from 'react-icons/fa'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import Userdetails from './Auth/UserDetails'
 import Image from 'next/image'
-
-const routes = [
-  { id: 1, label: 'Home', href: '/', Icon: FaHome },
-  { id: 2, label: 'Create Category', href: '/category', Icon: FaPlus },
-  { id: 3, label: 'Create Product', href: '/product', Icon: FaBox },
-  { id: 4, label: 'All Categories', href: '/categories', Icon: FaList },
-  { id: 5, label: 'All Products', href: '/products', Icon: FaBox },
-  { id: 6, label: 'All Users', href: '/users', Icon: FaUsers },
-]
-
+import { routes } from '@/utils/RoutesArray'
 const Sidebar = () => {
   const pathname = usePathname()
 
   return (
-    <div className="flex flex-col border-r-2 border-gray-200 text-gray-800 min-h-screen justify-normal lg:justify-between bg-gray-100 py-4 shadow-md">
+    <div className="flex flex-col border-r-2 border-gray-200 text-gray-800 min-h-screen justify-normal lg:justify-between bg-gray-900 py-4 shadow-md">
       <div className=" flex flex-col gap-5">
         <Link href={'/'} className="flex items-center gap-2 px-4 mb-6">
-          <Image src={'/Logo.png'} width={30} height={30} alt="Logo" />
-          <h6 className="text-lg font-extrabold text-gray-900">CharagDin</h6>
+          <Image src={'/logo.png'} width={150} height={30} alt="Logo" />
         </Link>
         <div className="flex flex-col gap-4 mx-2">
           {routes.map((route) => {
@@ -33,16 +22,16 @@ const Sidebar = () => {
                   href={route.href}
                   className={`flex gap-5 items-center p-2 rounded-lg ${
                     pathname === route.href
-                      ? 'bg-gray-300 text-blue-600'
-                      : 'text-gray-800 hover:bg-gray-200'
+                      ? 'bg-gray-700 text-purple-600'
+                      : 'text-purple-900 hover:bg-gray-700'
                   }`}
                 >
                   <Icon className="w-6 h-6" />
                   <h3
                     className={`text-sm ${
                       pathname === route.href
-                        ? 'text-blue-600'
-                        : 'text-gray-800'
+                        ? 'text-purple-600'
+                        : 'text-purple-900'
                     }`}
                   >
                     {route.label}
@@ -57,5 +46,4 @@ const Sidebar = () => {
     </div>
   )
 }
-
 export default Sidebar
