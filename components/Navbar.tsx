848fe1e4e110e8e768a32c47c6785bc9c1a33278
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-21 12:56:43 +0500

    needed to be fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,12 @@
 'use client'
-import { UserContext } from '@/utils/Context'
 import Image from 'next/image'
-import React, { useContext, useEffect, useState } from 'react'
+import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=" px-2 bg-slate-100 flex justify-between items-center p-1">
+    <div className=" px-2 bg-gray-900 flex justify-between items-center p-1">
       <div className=" flex items-center">
-        <Image src={'/Logo.png'} width={40} height={30} alt="Logo" />{' '}
-        <h6 className=" text-lg text-slate-800 font-extrabold">CharagDin</h6>
+        <Image src={'/logo.png'} width={100} height={100} alt="Logo" />{' '}
       </div>
 
       <h1 className=" text-lg text-slate-900 font-extralight">Admin Panel</h1>
