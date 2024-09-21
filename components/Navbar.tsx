af020cd9979174d8337c86fa192a0e344efc8243
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-21 13:12:23 +0500

    new navbar made with our style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=" px-2 bg-gray-900 flex justify-between items-center p-1">
+    <div className=" px-2 bg-gray-900 flex justify-between items-center p-4 px-6">
       <div className=" flex items-center">
-        <Image src={'/logo.png'} width={100} height={100} alt="Logo" />{' '}
+        <Image src={'/logo.png'} width={200} height={100} alt="Logo" />{' '}
       </div>
 
-      <h1 className=" text-lg text-slate-900 font-extralight">Admin Panel</h1>
+      <h1 className=" text-lg text-slate-100 font-extrabold">Admin Panel</h1>
     </div>
   )
 }
