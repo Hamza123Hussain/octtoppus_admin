555e6e4c5760165f59b62290f4090a9f0b9b095c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-21 13:13:01 +0500

    made it to our style

--- components/Auth/SignIn.tsx
+++ components/Auth/SignIn.tsx
@@ -28,7 +28,7 @@ const SignIn = () => {
 
   return (
     <div className="flex flex-col bg-gray-800 p-6 rounded-lg shadow-lg mx-auto max-w-md w-full sm:max-w-lg md:max-w-xl lg:max-w-2xl">
-      <h2 className="text-2xl sm:text-3xl font-semibold text-white mb-6 text-center">
+      <h2 className="text-2xl sm:text-3xl font-semibold text-purple-700 mb-6 text-center">
         Sign In
       </h2>
       <input
@@ -37,7 +37,7 @@ const SignIn = () => {
         name="email"
         value={inputVal.email}
         onChange={handleChange}
-        className="mb-4 p-3 w-full rounded bg-slate-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-300"
+        className="mb-4 p-3 w-full rounded bg-slate-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-300"
       />
       <input
         type="password"
@@ -45,16 +45,16 @@ const SignIn = () => {
         name="password"
         value={inputVal.password}
         onChange={handleChange}
-        className="mb-4 p-3 w-full rounded bg-slate-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-300"
+        className="mb-4 p-3 w-full rounded bg-slate-700 text-white-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-300"
       />
       <button
         onClick={HandleLogin}
-        className="bg-green-500 hover:bg-green-400 text-white font-semibold px-6 py-2 rounded transition-all w-full"
+        className="bg-purple-500 hover:bg-purple-400 text-purple-700 font-semibold px-6 py-2 rounded transition-all w-full"
       >
         Sign In
       </button>
       <div
-        className="flex justify-end text-white mt-2 hover:text-blue-900 cursor-pointer"
+        className="flex justify-end text-purple-700 mt-2 hover:text-purple-900 cursor-pointer"
         onClick={() => Router.push('/forgotpass')}
       >
         <span className="text-xs sm:text-sm">Forgot Your Password?</span>
@@ -63,7 +63,7 @@ const SignIn = () => {
         Don’t Have An Account?{' '}
         <span
           onClick={() => Router.push('/signup')}
-          className="underline cursor-pointer text-green-300 hover:text-green-200"
+          className="underline cursor-pointer text-purple-700 hover:text-purple-900"
         >
           Sign Up
         </span>
