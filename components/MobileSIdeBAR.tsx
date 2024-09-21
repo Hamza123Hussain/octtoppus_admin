1c7f17620ee0547762df44884a140db0481ec287
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-21 13:54:22 +0500

    fixed style a bit

--- components/MobileSIdeBAR.tsx
+++ components/MobileSIdeBAR.tsx
@@ -11,15 +11,20 @@ const MobileSideBar = () => {
     <div className=" flex items-center p-2 bg-gray-900 border-b-2  border-slate-700 ">
       <Sheet>
         <SheetTrigger>
-          <div className=" text-white  px-2 flex items-center gap-2">
-            <Menu size={40} />
-            <Image
-              className=" mt-2"
-              src={'/logo.png'}
-              width={200}
-              height={10}
-              alt="Logo"
-            />{' '}
+          <div className=" flex sm:flex-row flex-col justify-between items-center sm:w-[90vw]">
+            <div className=" text-white  px-2 flex   items-center gap-2">
+              <Menu size={40} />
+              <Image
+                className=" mt-2"
+                src={'/logo.png'}
+                width={200}
+                height={10}
+                alt="Logo"
+              />{' '}
+            </div>
+            <h1 className=" font-extrabold text-2xl text-purple-600 mt-4">
+              Admin Panel
+            </h1>
           </div>
         </SheetTrigger>
 
