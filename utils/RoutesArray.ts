df3ddf23d19fadb2f595791ef63619894abd1e65
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-21 15:17:55 +0500

    removed unused data

--- utils/RoutesArray.ts
+++ utils/RoutesArray.ts
@@ -4,5 +4,4 @@ export const routes = [
   { id: 1, label: 'Home', href: '/', Icon: FaHome },
   { id: 3, label: 'Create Blog', href: '/AddABlog', Icon: FaBox },
   { id: 5, label: 'All Blogs', href: '/GetBlogs', Icon: FaBox },
-  { id: 6, label: 'All Users', href: '/users', Icon: FaUsers },
 ]
